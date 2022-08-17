12bf601bb93a8793e78cd26b20137522c8052963
yilinng <wei9912@gmail.com>
2022-08-17 20:14:41 +0800

    wait for selector

--- tests/e2e/login.spec.js
+++ tests/e2e/login.spec.js
@@ -9,14 +9,14 @@ test('expect login success and logout success', async ({ page }) => {
     await page.locator('input[id="password"]').fill('test12');
     await page.locator('button[type="submit"]').click();
     // create a locator
-    const noteLink = page.locator('text=Notes');
+    //const noteLink = page.locator('text=Notes');
     // Expect an attribute "to be strictly equal" to the value.
-    await expect(noteLink).toHaveAttribute('href', '/notes')
+    //await expect(noteLink).toHaveAttribute('href', '/notes')
   
-    await page.locator('text=Logout').click();
+    //await page.locator('text=Logout').click();
 
     // Make a few checks that will not stop the test when failed...
-    await expect.soft(page.locator('.notice p')).toHaveText('You have been logged out.');    
+    //await expect.soft(page.locator('.notice p')).toHaveText('You have been logged out.');    
 
     await expect(page).not.toHaveURL(/.*notes/);    
 });
@@ -32,8 +32,9 @@ test('expect login failed', async ({ page }) => {
   await page.locator('button[type="submit"]').click();
 
   // Make a few checks that will not stop the test when failed...
-  const element = await page.waitForSelector('.alert p');
-  await element.toHaveText('Invalid email or password.');
+  //const element = await page.waitForSelector('.alert p');
+  //await element.toHaveText('Invalid email or password.');
+  await expect.soft(page.locator('.alert p')).toHaveText('Invalid email or password.');   
 });
 
 
